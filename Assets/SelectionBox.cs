e6a9af2b6ac7ab1026a0eb69137c4da6555706f7
TauntiO <titouan.br64@gmail.com>
2025-03-08 12:38:48 -0500

    fix: single selection

--- Assets/SelectionBox.cs
+++ Assets/SelectionBox.cs
@@ -36,8 +36,10 @@ public class UnitSelectionBox : MonoBehaviour
         // When Dragging
         if (Input.GetMouseButton(0))
         {
-            UnitSelectionManager.Instance.DeselectAll();
-            SelectUnits();
+            if (boxVisual.rect.width > 0 || boxVisual.rect.height > 0) {
+                UnitSelectionManager.Instance.DeselectAll();
+                SelectUnits();
+            }
 
             endPosition = Input.mousePosition;
             DrawVisual();
